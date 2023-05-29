dd7892476481a2dfb3c83b81529ae746a0fbd5f1
David-Tillmann Schaefer <codingdave@gmail.com>
2023-05-29 23:31:26 +0200

    trying perceptual hashes

--- SortPhotosWithXmpByExifDateCli/CommandLine.cs
+++ SortPhotosWithXmpByExifDateCli/CommandLine.cs
@@ -1,4 +1,7 @@
 using System.CommandLine;
+using System.ComponentModel;
+using System.Security.Cryptography;
+using CoenM.ImageHash.HashAlgorithms;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -60,6 +63,7 @@ internal class CommandLine
         AddCheckIfFileNameContainsDateDifferentToExifDatesCommand();
         AddFixExifDateByOffsetCommand();
         AddDeleteLonelyXmpCommand();
+        AddCheckForDuplicateImagesCommand();
         AddRearrangeByCameraManufacturerCommand();
         AddRearrangeBySoftwareCommand();
     }
@@ -174,6 +178,48 @@ internal class CommandLine
         deleteLeftoverXmpsCommand.SetHandler(DeleteLonelyXmps!, _sourceOption!, _forceOption);
 
         _rootCommand.AddCommand(deleteLeftoverXmpsCommand);
+    }
+
+    private void AddCheckForDuplicateImagesCommand()
+    {
+        void CheckForDuplicateImages()
+        {
+            var basePath = "/run/media/david/1.44.1-42962/";
+            var f1 = basePath + "Photos/Fotos/20150106/dsc_1491.JPG";
+            var f2 = basePath + "Photos/Fotos/20150601/dsc_1491.JPG";
+            // var hash1 = MD5.Create().ComputeHash(File.Open(f1, FileMode.Open));
+            // var hash2 = MD5.Create().ComputeHash(File.Open(f2, FileMode.Open));
+
+            var hashAlgorithm = new AverageHash();
+            // or one of the other available algorithms:
+            // var hashAlgorithm = new DifferenceHash();
+            // var hashAlgorithm = new PerceptualHash();
+
+            using var stream1 = File.OpenRead(f1);
+            ulong imageHash1 = hashAlgorithm.Hash(stream1);
+
+            using var stream2 = File.OpenRead(f2);
+            ulong imageHash2 = hashAlgorithm.Hash(stream2);
+
+            ulong hash1 = hashAlgorithm.Hash(imageStream1);
+            ulong hash2 = hashAlgorithm.Hash(imageStream2);
+
+            double percentageImageSimilarity = CompareHash.Similarity(hash1, hash2);
+
+            // 2015/01/06/dsc_1491.JPG and 2015/06/01/dsc_1491.JPG 
+            // Run(new FixExifDateByOffset(directory, (TimeSpan)offset, force));
+            // throw new NotImplementedException();
+        }
+
+        var checkForDuplicateImagesCommand = new Command(
+            "checkForDuplicateImagesCommand",
+            "Scan for images that are duplicates and remove them.")
+        {
+        };
+
+        checkForDuplicateImagesCommand.SetHandler(CheckForDuplicateImages!);
+
+        _rootCommand.AddCommand(checkForDuplicateImagesCommand);
     }
 
     private void AddRearrangeByCameraManufacturerCommand()
