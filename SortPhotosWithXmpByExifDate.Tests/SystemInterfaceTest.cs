0c5ac6d77b96a68b350fcc847ba84f8a15244558
David-Tillmann Schaefer <codingdave@gmail.com>
2023-11-05 21:38:51 +0100

    add SystemInterfaceTest

---
+++ SortPhotosWithXmpByExifDate.Tests/SystemInterfaceTest.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+
+using Moq;
+
+using SystemInterface.IO;
+
+using SystemWrapper.IO;
+
+using Xunit;
+
+namespace SortPhotosWithXmpByExifDate.Tests;
+
+public class SystemInterfaceTest
+{
+    [Fact]
+    public void TestMocking()
+    {
+        var directoryMock = new Mock<IDirectory>();
+        var path = @"/home/david/projects/SortPhotosWithXmpByExifDateCli";
+        var fileList = new List<string>() { @"c:\test.txt", @"c:\test2.txt" };
+        _ = directoryMock
+            .Setup(x => x.GetCurrentDirectory())
+            .Returns(path);
+
+        _ = directoryMock
+            .Setup(x => x.SetCurrentDirectory(It.IsAny<string>()));
+
+        _ = directoryMock
+            .Setup(x => x.EnumerateFiles(It.IsAny<string>()))
+            .Returns(new[] { @"c:\test.txt", @"c:\test2.txt" });
+
+        Assert.Equal(path, directoryMock.Object.GetCurrentDirectory());
+        Assert.Equal(fileList, directoryMock.Object.EnumerateFiles(@"path/does/not/matter"));
+    }
+
+    [Fact]
+    public void CheckWrapWorks()
+    {
+        // Arrange
+        var realDirectory = new DirectoryWrap();
+
+        var path = "/home";
+
+        // Act
+        realDirectory.SetCurrentDirectory(path);
+
+        // Assert
+        Assert.Equal(path, realDirectory.GetCurrentDirectory());
+    }
+}
