931beddf146c15acb3f41174128bbd53bde4a6ea
David-Tillmann Schaefer <codingdave@gmail.com>
2023-11-06 17:12:05 +0100

    add Extension for IEnumerable.ForEach

---
+++ SortPhotosWithXmpByExifDate.Cli/Base/IEnumerableExtensions.cs
@@ -0,0 +1,12 @@
+namespace SortPhotosWithXmpByExifDate.Cli.Extensions;
+
+public static class IEnumerableExtensions
+{
+    public static void ForEach<T>(this IEnumerable<T> collection, Action<T> action)
+    {
+        foreach(var item in collection)
+        {
+            action(item);
+        }
+    }
+}
