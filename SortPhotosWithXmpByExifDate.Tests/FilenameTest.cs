8e48b30186e32f9e79de85d2d80dbf1d3f8513ee
David-Tillmann Schaefer <codingdave@gmail.com>
2023-11-06 17:11:46 +0100

    add Filename.Test

---
+++ SortPhotosWithXmpByExifDate.Tests/FilenameTest.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+using Microsoft.Extensions.Logging;
+
+using Moq;
+
+using SixLabors.ImageSharp;
+
+using SortPhotosWithXmpByExifDate.Cli.Repository;
+
+using SystemInterface.IO;
+
+using Xunit;
+
+namespace FilenameTests;
+
+public class FilenameTests
+{
+    public FilenameTests()
+    {
+
+    }
+
+    [Fact]
+    public void GetAllImagesInCurrentDirectory()
+    {
+        var oldName = "foo/old.name";
+        var newName = "bar/new.name";
+
+        var image = new ImageFile(oldName);
+        Assert.Equal(oldName, image.OriginalFilename);
+        Assert.Equal(oldName, image.CurrentFilename);
+        Assert.Null(image.NewFilename);
+
+        image.NewFilename = newName;
+        Assert.Equal(oldName, image.OriginalFilename);
+        Assert.Equal(newName, image.CurrentFilename);
+        Assert.Equal(newName, image.NewFilename);
+    }
+}
